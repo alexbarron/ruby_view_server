b4c4a24a3a4f3b5aae7fc705c9670a73d95297d7
Alex Barron <alexbarron@gmail.com>
2015-03-19 16:46:49 -0700

    HW 1 done

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,13 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+amount = 63 * 83
+
+cheetos = ERB.new "I like cheetos so much I could eat <%= amount %> cheetos right now."
+puts cheetos.result(binding)
+
+lunch_dishes = ["eggs", "lentils", "broccoli"]
+
+mylunch = ERB.new "Today for lunch I ate <%= lunch_dishes[0] %>" 
+puts mylunch.result(binding)
